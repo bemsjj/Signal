6994162cb6658c94cdb413fbf16c7024e94993d0
Iando24 <iandoraian00@gmail.com>
2022-01-10 02:41:45 +0300

    Refactoring code

--- src/main/java/com/signal/api/controller/UserController.java
+++ src/main/java/com/signal/api/controller/UserController.java
@@ -1,50 +1,12 @@
 package com.signal.api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.signal.api.model.AuthenticationRequest;
-import com.signal.api.model.AuthenticationResponse;
-import com.signal.api.security.services.MyUserDetailsService;
-import com.signal.api.security.services.util.JwtUtil;
-
 @RestController
-
 public class UserController {
-	
-	@Autowired
-	private AuthenticationManager authencticationManager;
-	
-	@Autowired
-	private MyUserDetailsService userDetailsService;
-	
-	@Autowired
-	private JwtUtil jwtTokenUtil;
-	
-	@CrossOrigin(origins = "http://localhost:3000")
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-		try {
-			authencticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-			);
-		}catch (BadCredentialsException e) {
-			throw new Exception("Incorect username or password");
-		}
-		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-		final String jwt = jwtTokenUtil.generateToken(userDetails);
-		
-		return ResponseEntity.ok(new AuthenticationResponse(jwt));
-	}
 	
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public ResponseEntity<String> hello() { 
