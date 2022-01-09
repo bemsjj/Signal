2cfeb335f14d7c01eef3172c00f1257a23273e12
Iando24 <iandoraian00@gmail.com>
2022-01-09 22:25:53 +0300

    generation token

---
+++ src/main/java/com/signal/api/model/User.java
@@ -0,0 +1,67 @@
+package com.signal.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document
+public class User {
+	
+	@Id
+	private String id;
+	
+	@Field
+	private String firstName;
+	
+	@Field
+	private String lastName;
+
+	public User() {
+	}
+	
+	
+	public User(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "{"
+					+ "id=" + id + ", "
+					+ "firstName=" + firstName + ", "
+					+ "lastName=" + lastName + 
+				"}";
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/signal/api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.signal.api.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.signal.api.model.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
