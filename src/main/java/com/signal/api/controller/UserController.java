22814b181883c04f5e933de6f990e1750a92b5af
Iando24 <iandoraian00@gmail.com>
2022-01-10 00:01:47 +0300

    Cors modify

--- src/main/java/com/signal/api/controller/UserController.java
+++ src/main/java/com/signal/api/controller/UserController.java
@@ -7,10 +7,9 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.signal.api.model.AuthenticationRequest;
@@ -19,8 +18,7 @@ import com.signal.api.security.services.MyUserDetailsService;
 import com.signal.api.security.services.util.JwtUtil;
 
 @RestController
-@RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:3000")
+
 public class UserController {
 	
 	@Autowired
@@ -32,12 +30,14 @@ public class UserController {
 	@Autowired
 	private JwtUtil jwtTokenUtil;
 	
-	@GetMapping("/")
+	@CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	public String hello() { 
 		return "Hello user";
 	}
 	
-	@PostMapping("/login")
+	@CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
 		try {
 			authencticationManager.authenticate(
